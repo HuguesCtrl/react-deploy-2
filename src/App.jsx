005129f2526dae5976bcefdc7b396105da82abdb
Hugues Collet <hc.pro@outlook.fr>
2023-08-22 15:01:06 +0200

    Code source

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Mettre un site en ligne</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        deserunt cum debitis dolor quidem! Optio nisi consequatur iusto,
+        quisquam dicta numquam labore ducimus sequi modi facilis cumque quas
+        commodi dolore tenetur. In commodi unde ex.
+      </p>
+      <img src={reactLogo} alt="React" width={200} />
+    </>
+  );
+}
+
+export default App;
